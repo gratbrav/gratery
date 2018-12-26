9d8cf73334c6424ac137595c4baa3b24d9632cd6
gratbrav <spam@huxsohl.de>
2018-12-26 14:52:35 +0100

    add local configuration

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # gratery
 
 simple lottery counter
+
+## local configuration
+
+to use local configuration rename `config.local.php.dist` to `config.local.php` and follow the instruction 
+within the file
--- config.php
+++ config.php
@@ -5,3 +5,7 @@ $config = [
     'currentYear' => date('Y'),
     'url' => 'https://www.lottozahlenonline.de/statistik/lotto-am-samstag/lottozahlen-archiv.php?j=',
 ];
+
+if (file_exists('config.local.php')) {
+    include_once 'config.local.php';
+}
